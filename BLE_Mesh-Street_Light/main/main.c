f85eb6dff771da689ce0d549f2c42dd4c0306909
Ata ur Rehman <ata.rehman@live.com>
2025-07-29 15:00:48 +0500

    Refactor Bluetooth configuration to enable NimBLE and disable Bluedroid
    - Enabled NimBLE support and configured memory allocation settings.
    - Set maximum connections, bonds, and CCCDs for NimBLE.
    - Adjusted task stack sizes and roles for NimBLE.
    - Updated GAP service settings, including device name and appearance.
    - Enabled BLE Mesh Friend support and configured related parameters.
    - Enabled various BLE Mesh server models, including Sensor, Time Scene, and Lighting servers.
    - Updated Wi-Fi provisioning settings to enable BLE secure connection.

--- BLE_Mesh-Street_Light/main/main.c
+++ BLE_Mesh-Street_Light/main/main.c
@@ -25,24 +25,22 @@
 #include "board.h"
 #include "ble_mesh_example_init.h"
 
-#include "hdc2080.h"
-#include "i2c_wrapper.h"
 #define TAG "EXAMPLE"
 
 #define CID_ESP 0x02E5
+
+#define ESP_BLE_MESH_MODEL_OP_BATTERY_STATUS    ESP_BLE_MESH_MODEL_OP_3(0x02, CID_ESP)
+#define ESP_BLE_MESH_VND_MODEL_OP_SEND          ESP_BLE_MESH_MODEL_OP_3(0x00, CID_ESP)
+#define ESP_BLE_MESH_VND_MODEL_OP_STATUS        ESP_BLE_MESH_MODEL_OP_3(0x01, CID_ESP)
 
 extern struct _led_state led_state[3];
 
 static uint8_t dev_uuid[16] = { 0xdd, 0xdd };
 
-i2c_master_bus_handle_t i2c_bus_handle = NULL;
-i2c_master_dev_handle_t mst_dev_handle = NULL; 
-
 static esp_ble_mesh_cfg_srv_t config_server = {
     /* 3 transmissions with 20ms interval */
     .net_transmit = ESP_BLE_MESH_TRANSMIT(2, 20),
- // .relay = ESP_BLE_MESH_RELAY_DISABLED,   
-    .relay = ESP_BLE_MESH_RELAY_ENABLED,
+    .relay = ESP_BLE_MESH_RELAY_DISABLED,
     .relay_retransmit = ESP_BLE_MESH_TRANSMIT(2, 20),
     .beacon = ESP_BLE_MESH_BEACON_ENABLED,
 #if defined(CONFIG_BLE_MESH_GATT_PROXY_SERVER)
@@ -57,7 +55,7 @@ static esp_ble_mesh_cfg_srv_t config_server = {
 #endif
     .default_ttl = 7,
 };
-    
+
 ESP_BLE_MESH_MODEL_PUB_DEFINE(onoff_pub_0, 2 + 3, ROLE_NODE);
 static esp_ble_mesh_gen_onoff_srv_t onoff_server_0 = {
     .rsp_ctrl = {
@@ -66,39 +64,38 @@ static esp_ble_mesh_gen_onoff_srv_t onoff_server_0 = {
     },
 };
 
-ESP_BLE_MESH_MODEL_PUB_DEFINE(onoff_pub_1, 2 + 3, ROLE_NODE);
-static esp_ble_mesh_gen_onoff_srv_t onoff_server_1 = {
-    .rsp_ctrl = {
-        .get_auto_rsp = ESP_BLE_MESH_SERVER_RSP_BY_APP,
-        .set_auto_rsp = ESP_BLE_MESH_SERVER_RSP_BY_APP,
-    },
-};
-
-ESP_BLE_MESH_MODEL_PUB_DEFINE(onoff_pub_2, 2 + 3, ROLE_NODE);
-static esp_ble_mesh_gen_onoff_srv_t onoff_server_2 = {
-    .rsp_ctrl = {
-        .get_auto_rsp = ESP_BLE_MESH_SERVER_AUTO_RSP,
-        .set_auto_rsp = ESP_BLE_MESH_SERVER_RSP_BY_APP,
-    },
-};
-
 static esp_ble_mesh_model_t root_models[] = {
     ESP_BLE_MESH_MODEL_CFG_SRV(&config_server),
     ESP_BLE_MESH_MODEL_GEN_ONOFF_SRV(&onoff_pub_0, &onoff_server_0),
 };
 
-static esp_ble_mesh_model_t extend_model_0[] = {
-    ESP_BLE_MESH_MODEL_GEN_ONOFF_SRV(&onoff_pub_1, &onoff_server_1),
+ESP_BLE_MESH_MODEL_PUB_DEFINE(battery_pub, 2 + 4, ROLE_NODE); // 2 opcode + 4 voltage data
+static esp_ble_mesh_model_op_t battery_op[] = {
+    // {
+    //     ESP_BLE_MESH_MODEL_OP_BATTERY_STATUS, 0 
+    // }, // No payload expected in GET (or define your own GET)
+    ESP_BLE_MESH_MODEL_OP(ESP_BLE_MESH_MODEL_OP_BATTERY_STATUS, 2),
+    ESP_BLE_MESH_MODEL_OP_END,
 };
 
-static esp_ble_mesh_model_t extend_model_1[] = {
-    ESP_BLE_MESH_MODEL_GEN_ONOFF_SRV(&onoff_pub_2, &onoff_server_2),
+ESP_BLE_MESH_MODEL_PUB_DEFINE(vnd_pub, 2 + 4, ROLE_NODE); // 2 opcode + 4 voltage data
+static esp_ble_mesh_model_op_t vnd_op[] = {
+    ESP_BLE_MESH_MODEL_OP(ESP_BLE_MESH_VND_MODEL_OP_SEND, 2),
+    ESP_BLE_MESH_MODEL_OP_END,
+};
+
+static esp_ble_mesh_model_t battery_model[] = {
+    ESP_BLE_MESH_VENDOR_MODEL(CID_ESP, 0x0001, battery_op, &battery_pub, NULL),
+    // ESP_BLE_MESH_VENDOR_MODEL(CID_ESP, 0x0001, vnd_op, NULL, NULL),
+};
+
+static esp_ble_mesh_model_t vnd_models[] = {
+    ESP_BLE_MESH_VENDOR_MODEL(CID_ESP, 0x0001, vnd_op, &vnd_pub, NULL),
 };
 
 static esp_ble_mesh_elem_t elements[] = {
-    ESP_BLE_MESH_ELEMENT(0, root_models, ESP_BLE_MESH_MODEL_NONE),
-    ESP_BLE_MESH_ELEMENT(0, extend_model_0, ESP_BLE_MESH_MODEL_NONE),
-    ESP_BLE_MESH_ELEMENT(0, extend_model_1, ESP_BLE_MESH_MODEL_NONE),
+    ESP_BLE_MESH_ELEMENT(0, root_models, battery_model),
+    // ESP_BLE_MESH_ELEMENT(0, battery_model, vnd_models),
 };
 
 static esp_ble_mesh_comp_t composition = {
@@ -135,22 +132,16 @@ static void example_change_led_state(esp_ble_mesh_model_t *model,
     uint8_t elem_count = esp_ble_mesh_get_element_count();
     struct _led_state *led = NULL;
     uint8_t i;
-    
-    if (ESP_BLE_MESH_ADDR_IS_UNICAST(ctx->recv_dst)) {
-        led = &led_state[model->element->element_addr - primary_addr];
-        board_led_operation(led->pin, onoff);
-        ESP_LOGI(TAG, "Toggling LED on GPIO %d to %s", led->pin, onoff ? "ON" : "OFF");
 
-        // for (i = 0; i < elem_count; i++) {
-        //     if (ctx->recv_dst == (primary_addr + i)) {
-        //         led = &led_state[i];
-        //         board_led_operation(led->pin, onoff);
-        //     }
-        // }
+    if (ESP_BLE_MESH_ADDR_IS_UNICAST(ctx->recv_dst)) {
+        for (i = 0; i < elem_count; i++) {
+            if (ctx->recv_dst == (primary_addr + i)) {
+                led = &led_state[i];
+                board_led_operation(led->pin, onoff);
+            }
+        }
     } else if (ESP_BLE_MESH_ADDR_IS_GROUP(ctx->recv_dst)) {
         if (esp_ble_mesh_is_model_subscribed_to_group(model, ctx->recv_dst)) {
-    
-            ESP_LOGI("ONOF_CHECK", "Model subscribed to group 0x%04x", ctx->recv_dst);
             led = &led_state[model->element->element_addr - primary_addr];
             board_led_operation(led->pin, onoff);
         }
@@ -158,14 +149,11 @@ static void example_change_led_state(esp_ble_mesh_model_t *model,
         led = &led_state[model->element->element_addr - primary_addr];
         board_led_operation(led->pin, onoff);
     }
-    else{
-        ESP_LOGE(TAG, "Invalid destination address: 0x%04x", ctx->recv_dst);
-    }
 }
 
 static void example_handle_gen_onoff_msg(esp_ble_mesh_model_t *model,
-                                            esp_ble_mesh_msg_ctx_t *ctx,
-                                            esp_ble_mesh_server_recv_gen_onoff_set_t *set)
+                                         esp_ble_mesh_msg_ctx_t *ctx,
+                                         esp_ble_mesh_server_recv_gen_onoff_set_t *set)
 {
     esp_ble_mesh_gen_onoff_srv_t *srv = (esp_ble_mesh_gen_onoff_srv_t *)model->user_data;
 
@@ -238,8 +226,7 @@ static void example_ble_mesh_generic_server_cb(esp_ble_mesh_generic_server_cb_ev
 
     switch (event) {
     case ESP_BLE_MESH_GENERIC_SERVER_STATE_CHANGE_EVT:
-        ESP_LOGI(TAG, "Button Press");
-        example_change_led_state(param->model, &param->ctx, param->value.state_change.onoff_set.onoff);
+        ESP_LOGI(TAG, "ESP_BLE_MESH_GENERIC_SERVER_STATE_CHANGE_EVT");
         if (param->ctx.recv_op == ESP_BLE_MESH_MODEL_OP_GEN_ONOFF_SET ||
             param->ctx.recv_op == ESP_BLE_MESH_MODEL_OP_GEN_ONOFF_SET_UNACK) {
             ESP_LOGI(TAG, "onoff 0x%02x", param->value.state_change.onoff_set.onoff);
@@ -306,6 +293,39 @@ static void example_ble_mesh_config_server_cb(esp_ble_mesh_cfg_server_cb_event_t
     }
 }
 
+
+static void example_ble_mesh_custom_model_cb(esp_ble_mesh_model_cb_event_t event,
+                                             esp_ble_mesh_model_cb_param_t *param)
+{
+    switch (event) {
+    case ESP_BLE_MESH_MODEL_OPERATION_EVT:
+        if (param->model_operation.opcode == ESP_BLE_MESH_VND_MODEL_OP_SEND) {
+            uint16_t tid = *(uint16_t *)param->model_operation.msg;
+            ESP_LOGI(TAG, "Recv 0x%06" PRIx32 ", tid 0x%04x", param->model_operation.opcode, tid);
+            esp_err_t err = esp_ble_mesh_server_model_send_msg(&vnd_models[0],
+                    param->model_operation.ctx, ESP_BLE_MESH_VND_MODEL_OP_STATUS,
+                    sizeof(tid), (uint8_t *)&tid);
+            err = esp_ble_mesh_server_model_send_msg(&battery_model[0],
+                    param->model_operation.ctx, ESP_BLE_MESH_MODEL_OP_BATTERY_STATUS,
+                    sizeof(tid), (uint8_t *)&tid);
+            if (err) {
+                ESP_LOGE(TAG, "Failed to send message 0x%06x", ESP_BLE_MESH_VND_MODEL_OP_STATUS);
+                // ESP_LOGE(TAG, "Failed to send message 0x%06x", ESP_BLE_MESH_MODEL_OP_BATTERY_STATUS);
+            }
+        }
+        break;
+    case ESP_BLE_MESH_MODEL_SEND_COMP_EVT:
+        if (param->model_send_comp.err_code) {
+            ESP_LOGE(TAG, "Failed to send message 0x%06" PRIx32, param->model_send_comp.opcode);
+            break;
+        }
+        ESP_LOGI(TAG, "Send 0x%06" PRIx32, param->model_send_comp.opcode);
+        break;
+    default:
+        break;
+    }
+}
+
 static esp_err_t ble_mesh_init(void)
 {
     esp_err_t err = ESP_OK;
@@ -313,6 +333,7 @@ static esp_err_t ble_mesh_init(void)
     esp_ble_mesh_register_prov_callback(example_ble_mesh_provisioning_cb);
     esp_ble_mesh_register_config_server_callback(example_ble_mesh_config_server_cb);
     esp_ble_mesh_register_generic_server_callback(example_ble_mesh_generic_server_cb);
+    esp_ble_mesh_register_custom_model_callback(example_ble_mesh_custom_model_cb);
 
     err = esp_ble_mesh_init(&provision, &composition);
     if (err != ESP_OK) {
@@ -333,45 +354,6 @@ static esp_err_t ble_mesh_init(void)
     return err;
 }
 
-
-// void HDC_task(){        
-// float temperature= 0.0f,  humidity = 0.0f;
-
-//     esp_err_t err = hdc2080_begin(i2c_bus_handle, &mst_dev_handle, HDC2080_ADDR);
-//     if (err == ESP_OK) {
-//         printf("HDC2080 sensor found\n");
-//     }
-//     else if (err == ESP_ERR_INVALID_RESPONSE) {
-//         printf("HDC2080 sensor not found\n");
-//     }
-//     // hdc2080_reset(mst_dev_handle);  
-//     vTaskDelay(pdMS_TO_TICKS(500));
-//     hdc2080_set_measurement_mode(mst_dev_handle, TEMP_AND_HUMID);
-//     hdc2080_set_rate(mst_dev_handle, MANUAL);
-//     hdc2080_set_temp_resolution(mst_dev_handle, FOURTEEN_BIT);
-//     hdc2080_set_humid_resolution(mst_dev_handle, FOURTEEN_BIT);
-//     hdc2080_trigger_measurement(mst_dev_handle);
-//     vTaskDelay(pdMS_TO_TICKS(100));
-//    while (1) {
-       
-//         // char temp_text[20] = {0};
-//         // char humid_text[20] = {0};
-//         hdc2080_trigger_measurement(mst_dev_handle);  // Ensure a new measurement is taken
-//         vTaskDelay(pdMS_TO_TICKS(100)); // Allow time for measurement
-    
-//         hdc2080_read_temp(mst_dev_handle, &temperature);
-//         hdc2080_read_humidity(mst_dev_handle, &humidity);
-    
-//         printf("Temperature: %.2f °C, Humidity: %.2f\n", temperature, humidity);
-        
-//         //  // Format temperature and humidity
-//         // sprintf(temp_text, "Temp: %.1f C", temperature);
-//         // sprintf(humid_text, "Hum:  %.1f %%", humidity);
-
-    
-//          vTaskDelay(pdMS_TO_TICKS(5000));  // Read every 5 seconds
-//     }
-// }
 void app_main(void)
 {
     esp_err_t err;
@@ -380,17 +362,6 @@ void app_main(void)
 
     board_init();
 
-    i2c_master_bus_config_t i2c_mst_config = {
-		.clk_source = I2C_CLK_SRC_DEFAULT,
-		.glitch_ignore_cnt = 7,
-		.i2c_port = I2C_NUM,
-		.scl_io_num =  22,
-		.sda_io_num =  21,
-		.flags.enable_internal_pullup = true,
-	};
-   
-	ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &i2c_bus_handle));
-     
     err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
         ESP_ERROR_CHECK(nvs_flash_erase());
@@ -403,7 +374,7 @@ void app_main(void)
         ESP_LOGE(TAG, "esp32_bluetooth_init failed (err %d)", err);
         return;
     }
-        
+
     ble_mesh_get_dev_uuid(dev_uuid);
 
     /* Initialize the Bluetooth Mesh Subsystem */
@@ -411,11 +382,4 @@ void app_main(void)
     if (err) {
         ESP_LOGE(TAG, "Bluetooth mesh init failed (err %d)", err);
     }
-    ESP_LOGI(TAG, "Bluetooth Mesh initialized");
-    // HDC_task();
-    // ESP_LOGI(TAG, "HDC2080 task started");
-    // while (1) { 
-    //     vTaskDelay(pdMS_TO_TICKS(1000));  // Main loop delay
-    // }
-    
 }
